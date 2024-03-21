dfa2a23ca88b367c1e002176ed3a150335837a32
Julie Poulain <juliempoulain@gmail.com>
2024-03-20 18:16:12 -0700

    adjusted hierarchy, re-arranged everything

--- src/index.js
+++ src/index.js
@@ -1,48 +1,65 @@
-const addSubmitListener = (event) => {
-  event.preventDefault();
-  const newName = event.target.name.value;
-  const newImage = event.target.image.value;
-  const ramenMenuDiv = document.querySelector("#ramen-menu");
-  const newRamenImg = document.createElement("img");
-  newRamenImg.src = newImage;
-  newRamenImg.alt = newName;
-  ramenMenuDiv.append(newRamenImg);
-};
-
-const newRamenForm = document.querySelector("#new-ramen");
-newRamenForm.addEventListener("submit", addSubmitListener);
-
-const data = fetch(`http://localhost:3000/ramens/`)
-  .then((r) => r.json())
-
-
-
+//callback functions
 const displayRamens = () => {
-  for (let i = 1; i < 6; i++) {
-    fetch(`http://localhost:3000/ramens/${i}`)
-      .then((r) => r.json())
-      .then((ramenData) => {
+  fetch(`http://localhost:3000/ramens/`)
+    .then((r) => r.json())
+    .then((ramens) => {
+      for (const ramen of ramens) {
         const ramenMenuDiv = document.querySelector("#ramen-menu");
         const ramenImg = document.createElement("img");
-        ramenImg.src = ramenData.image;
-        ramenImg.alt = ramenData.name;
-        ramenImg.id = ramenData.id;
+        ramenImg.src = ramen.image;
+        ramenImg.alt = ramen.name;
+        ramenImg.id = ramen.id;
         ramenMenuDiv.append(ramenImg);
-        ramenImg.addEventListener("click", function handleClick() {
-          const detailImage = document.querySelector(".detail-image");
-          detailImage.src = ramenData.image;
-          detailImage.alt = ramenData.name;
-          const detailName = document.querySelector(".name");
-          detailName.textContent = ramenData.name;
-          const detailRestaurant = document.querySelector(".restaurant");
-          detailRestaurant.textContent = ramenData.restaurant;
-          const detailRating = document.querySelector("#rating-display");
-          detailRating.textContent = ramenData.rating;
-          const detailComment = document.querySelector("#comment-display");
-          detailComment.textContent = ramenData.comment;
+        ramenImg.addEventListener("click", () => {
+          handleClick(ramen);
         });
-      });
-  }
+      }
+      handleClick(ramens);
+    });
+};
+
+const handleClick = (ramen) => {
+  const detailImage = document.querySelector(".detail-image");
+  detailImage.src = ramen.image;
+  detailImage.alt = ramen.name;
+  const detailName = document.querySelector(".name");
+  detailName.textContent = ramen.name;
+  const detailRestaurant = document.querySelector(".restaurant");
+  detailRestaurant.textContent = ramen.restaurant;
+  const detailRating = document.querySelector("#rating-display");
+  detailRating.textContent = ramen.rating;
+  const detailComment = document.querySelector("#comment-display");
+  detailComment.textContent = ramen.comment;
+};
+
+const addSubmitListener = () => {
+  const form = document.querySelector("#new-ramen");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formRamen = {
+      name: e.target.name.value,
+      restaurant: e.target.restaurant.value,
+      image: e.target.image.value,
+      rating: e.target.rating.value,
+      //including the comment value below causes the submission event to fail - need to continue to debug
+      // comment: e.target.comment.value,
+    };
+    const postRamen = () => {
+      fetch("http://localhost:3000/ramens", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(formRamen),
+      })
+        .then((r) => r.json())
+        .then((ramens) => {
+          console.log(ramens);
+        });
+    };
+    postRamen();
+  });
 };
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -51,4 +68,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
 const main = () => {
   displayRamens();
+  addSubmitListener();
 };
+
+//Export functions for testing
+export { displayRamens, addSubmitListener, handleClick, main };
